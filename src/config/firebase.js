d7aa96a5d2737f1360caaba296022405f20f2726
victorbathich <159079590+victorbathich@users.noreply.github.com>
2026-05-14 13:36:01 -0300

    configurando banco de dados

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCNTiLSkwxOcvw8OYBABii08447AdvOkZo',
-  authDomain: 'controle-de-medicamentos-fc6ab.firebaseapp.com',
-  projectId: 'controle-de-medicamentos-fc6ab',
-  storageBucket: 'controle-de-medicamentos-fc6ab.firebasestorage.app',
-  messagingSenderId: '162059162201',
-  appId: '1:162059162201:web:a652dee00b81bb8004d748',
+  apiKey: 'AIzaSyCl3ZyyV2ek8qVWlxDHMynqeEXKbgwyAmQ',
+  authDomain: 'medicare-81917.firebaseapp.com',
+  projectId: 'medicare-81917',
+  storageBucket: 'medicare-81917.firebasestorage.app',
+  messagingSenderId: '821902205281',
+  appId: '1:821902205281:web:e7e61573eb76a1881fe520',
+  measurementId: 'G-XRC83KVV9N',
 };
 
 export const firebaseConfigurado = true;
